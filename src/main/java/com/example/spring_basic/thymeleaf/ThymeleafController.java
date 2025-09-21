926763e6dca0b2886a7923c1758da7beca0f743a
tsuzuki-privateDev <tsuzuki2215@gmail.com>
2025-09-21 20:29:06 +0900

    Thymeleafの色々な使い方

---
+++ src/main/java/com/example/spring_basic/thymeleaf/ThymeleafController.java
@@ -0,0 +1,46 @@
+package com.example.spring_basic.thymeleaf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ThymeleafController {
+    // 「/thymeleaf」へアクセスがあった場合
+    @GetMapping("/thymeleaf")
+    public ModelAndView thymeleaf(ModelAndView mav) {
+        // リンク用サンプル
+        mav.addObject("user_id", 999);
+        // 選択オブジェクト用サンプル
+        User user = new User("神田太郎", 25);
+        mav.addObject("user", user);
+        // HTMLコード出力用サンプル
+        mav.addObject("html","<h3>HTMLコードを出力しました。</h3>");
+        mav.setViewName("thymeleaf/howto");
+        return mav;
+    }
+}
+
+class User {
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
